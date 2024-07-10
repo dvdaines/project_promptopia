56d95fae4e2b451ef1c2dd76cfb3c3a99c3d46cf
dvdaines <ddaines@protonmail.com>
2024-07-10 10:36:51 -0500

    wrapped useSearchParams in suspense tag to fix error

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,11 +1,19 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
 
 const EditPrompt = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <EditPromptContent />
+    </Suspense>
+  );
+};
+
+const EditPromptContent = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
 
